85fc2af030ceeeaebd5b05f29a89e52a74688941
kubersharma001 <kubersharma001@gmail.com>
2021-08-27 17:25:10 +0000

    vayu: Import powerhint.json from coral
    * From: https://android.googlesource.com/device/google/coral/+/android-11.0.0_r42/powerhint.json
    
    Change-Id: I8dba2912afb27e0432bdce601ce38f2934bb0186

--- device.mk
+++ device.mk
@@ -316,6 +316,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # QTI
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor
